2a034250cad1e02cf5e0de721f051f83bf3b44eb
Bryan W. Weber <bryan.w.weber@gmail.com>
2018-06-28 21:22:35 -0400

    Add tutorials link to navbar

--- conf.py
+++ conf.py
@@ -135,6 +135,7 @@ TRANSLATIONS_PATTERN = '{path}.{lang}.{ext}'
 NAVIGATION_LINKS = {
     DEFAULT_LANG: (
         ('/install/index.html', 'Install'),
+        ('/tutorials/index.html', 'Tutorials'),
         ('/examples/index.html', 'Examples'),
         ('/community.html', 'Community'),
         ('/science/index.html', 'Science'),
