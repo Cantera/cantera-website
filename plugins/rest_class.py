32d597c533c59c57fd73a6f3829261ed2d499887
Bryan W. Weber <bryan.w.weber@gmail.com>
2018-11-09 17:29:24 -0500

    Apply the black formatter to the rest_class plugin

--- plugins/rest_class.py
+++ plugins/rest_class.py
@@ -14,25 +14,25 @@ from nikola.utils import split_explicit_title, get_logger
 from nikola.plugin_categories import RestExtension
 
 
-LOGGER = get_logger('rest_class')
+LOGGER = get_logger("rest_class")
 
 
 class Plugin(RestExtension):
     """Plugin for :class: and :func: role."""
 
-    name = 'rest_class'
+    name = "rest_class"
 
     def set_site(self, site):
         """Set the Nikola site instance for this plugin."""
         self.site = site
-        roles.register_canonical_role('class', class_role)
-        roles.register_canonical_role('cti:class', class_role)
-        roles.register_canonical_role('py:class', class_role)
-        roles.register_canonical_role('mat:class', class_role)
-        roles.register_canonical_role('func', class_role)
-        roles.register_canonical_role('cti:func', class_role)
-        roles.register_canonical_role('py:func', class_role)
-        roles.register_canonical_role('mat:func', class_role)
+        roles.register_canonical_role("class", class_role)
+        roles.register_canonical_role("cti:class", class_role)
+        roles.register_canonical_role("py:class", class_role)
+        roles.register_canonical_role("mat:class", class_role)
+        roles.register_canonical_role("func", class_role)
+        roles.register_canonical_role("cti:func", class_role)
+        roles.register_canonical_role("py:func", class_role)
+        roles.register_canonical_role("mat:func", class_role)
         class_role.site = site
         return super(Plugin, self).set_site(site)
 
@@ -42,15 +42,15 @@ def _class_link(name, rawtext, text):
     if class_role.site.processing_targets:
         return True, True, None, None, None, None
 
-    context_map = {'py': 'cython', 'mat': 'matlab', 'cti': 'cti'}
-    context = name.split(':', 1)[0]
+    context_map = {"py": "cython", "mat": "matlab", "cti": "cti"}
+    context = name.split(":", 1)[0]
     if context == name:
-        context = class_role.site.config['DEFAULT_CONTEXT']
+        context = class_role.site.config["DEFAULT_CONTEXT"]
         default_context = True
     else:
         default_context = False
 
-    target = '{}_targets'.format(context_map[context])
+    target = "{}_targets".format(context_map[context])
 
     if class_role.site.cache.get(target) is not None:
         targets = class_role.site.cache.get(target).copy()
@@ -60,15 +60,18 @@ def _class_link(name, rawtext, text):
     has_explicit_title, title, label = split_explicit_title(text)
 
     if label not in targets and not default_context:
-        LOGGER.error('The label {label} was not found in the context {context}'.format(
-            label=label, context=context))
+        LOGGER.error(
+            "The label {label} was not found in the context {context}".format(
+                label=label, context=context
+            )
+        )
         return False, False, None, None, None, label
     elif label not in targets and default_context:
         c_map = context_map.copy()
         c_map.pop(context)
         found_label = False
         for context, t in c_map.items():
-            target = '{}_targets'.format(t)
+            target = "{}_targets".format(t)
             if class_role.site.cache.get(target) is not None:
                 targets = class_role.site.cache.get(target).copy()
             else:
@@ -78,18 +81,24 @@ def _class_link(name, rawtext, text):
                 continue
             else:
                 found_label = True
-                LOGGER.info('The label {} was found in the context {}. Consider explicitly '
-                            'specifying the context for this link'.format(label, context))
+                LOGGER.info(
+                    "The label {} was found in the context {}. Consider explicitly "
+                    "specifying the context for this link".format(label, context)
+                )
                 break
 
         if not found_label:
-            LOGGER.error('The label {} could not be found in any context'.format(label))
+            LOGGER.error("The label {} could not be found in any context".format(label))
             return False, False, None, None, None, label
 
     doc_file = targets[label][0]
-    permalink = '/documentation/docs-{}/'.format(class_role.site.config['CANTERA_VERSION'])
-    permalink += doc_file + '#' + targets[label][1]
-    code_node = nodes.literal(rawtext, targets[label][2] + '()', classes=['code', 'xref', context])
+    permalink = "/documentation/docs-{}/".format(
+        class_role.site.config["CANTERA_VERSION"]
+    )
+    permalink += doc_file + "#" + targets[label][1]
+    code_node = nodes.literal(
+        rawtext, targets[label][2] + "()", classes=["code", "xref", context]
+    )
     if not has_explicit_title:
         title = targets[label][1]
 
@@ -98,21 +107,24 @@ def _class_link(name, rawtext, text):
 
 def class_role(name, rawtext, text, lineno, inliner, options={}, content=[]):
     """Handle the class role."""
-    success, processing, title, code_node, permalink, label = _class_link(name, rawtext, text)
+    success, processing, title, code_node, permalink, label = _class_link(
+        name, rawtext, text
+    )
     if processing:
-        return [nodes.raw('', text, format='html')], []
+        return [nodes.raw("", text, format="html")], []
     if success:
         node = make_link_node(rawtext, title, code_node, permalink, options)
         return [node], []
     else:
         msg = inliner.reporter.warning(
-            'The label {0} was not found'.format(label), line=lineno)
+            "The label {0} was not found".format(label), line=lineno
+        )
         prb = inliner.problematic(rawtext, rawtext, msg)
         return [prb], [msg]
 
 
 def make_link_node(rawtext, text, code_node, url, options):
     """Make a docutils link node."""
-    node = nodes.reference('', '', refuri=url, *options)
+    node = nodes.reference("", "", refuri=url, *options)
     node.append(code_node)
     return node
