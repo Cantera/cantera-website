9dc7c5a859788d601c01330a1c87e89f6117109a
Bryan W. Weber <bryan.w.weber@gmail.com>
2019-03-19 09:10:56 -0400

    Delete the MathJax configuration options
    We only use KaTeX anyways, so simplify the handling code

--- conf.py
+++ conf.py
@@ -1053,49 +1053,6 @@ PRETTY_URLS = False
 # If True, use the scheduling rule to all posts (not pages!) by default
 # SCHEDULE_ALL = False
 
-# Do you want a add a Mathjax config file?
-# MATHJAX_CONFIG = ""
-
-# If you want support for the $.$ syntax (which may conflict with running
-# text!), just use this config:
-# MATHJAX_CONFIG = """
-# <script type="text/x-mathjax-config">
-# MathJax.Hub.Config({
-#     tex2jax: {
-#         inlineMath: [ ['$','$'], ["\\\(","\\\)"] ],
-#         displayMath: [ ['$$','$$'], ["\\\[","\\\]"] ],
-#         processEscapes: true
-#     },
-#     displayAlign: 'center', // Change this to 'left' if you want left-aligned equations.
-#     "HTML-CSS": {
-#         styles: {'.MathJax_Display': {"margin": 0}}
-#     }
-# });
-# </script>
-# """
-
-# Want to use KaTeX instead of MathJax? While KaTeX may not support every
-# feature yet, it's faster and the output looks better.
-USE_KATEX = True
-
-# KaTeX auto-render settings. If you want support for the $.$ syntax (wihch may
-# conflict with running text!), just use this config:
-# KATEX_AUTO_RENDER = """
-# delimiters: [
-#     {left: "$$", right: "$$", display: true},
-#     {left: "\\\\[", right: "\\\\]", display: true},
-#     {left: "\\\\begin{equation*}", right: "\\\\end{equation*}", display: true},
-#     {left: "$", right: "$", display: false},
-#     {left: "\\\\(", right: "\\\\)", display: false}
-# ]
-# """
-
-# Do you want to customize the nbconversion of your IPython notebook?
-# IPYNB_CONFIG = {}
-# With the following example configuration you can use a custom jinja template
-# called `toggle.tpl` which has to be located in your site/blog main folder:
-# IPYNB_CONFIG = {'Exporter':{'template_file': 'toggle'}}
-
 # What Markdown extensions to enable?
 # You will also get gist, nikola and podcast because those are
 # done in the code, hope you don't mind ;-)
