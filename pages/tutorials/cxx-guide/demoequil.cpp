4f96f2fc66a3892ab6c114eafe54116818cc4bff
Ingmar Schoegl <ischoegl@lsu.edu>
2022-04-04 13:05:50 -0500

    Use newSolution rather than newPhase in tutorials

--- pages/tutorials/cxx-guide/demoequil.cpp
+++ pages/tutorials/cxx-guide/demoequil.cpp
@@ -1,3 +1,4 @@
+#include "cantera/base/Solution.h"
 #include "cantera/thermo.h"
 #include <iostream>
 
@@ -5,7 +6,10 @@ using namespace Cantera;
 
 void equil_demo()
 {
-    std::unique_ptr<ThermoPhase> gas(newPhase("h2o2.yaml"));
+    // Create a new Solution object
+    auto sol = newSolution("h2o2.yaml", "ohmech", "None");
+    auto gas = sol->thermo();
+
     gas->setState_TPX(1500.0, 2.0*OneAtm, "O2:1.0, H2:3.0, AR:1.0");
     gas->equilibrate("TP");
     std::cout << gas->report() << std::endl;
--- pages/tutorials/cxx-guide/factory_demo.cpp
+++ pages/tutorials/cxx-guide/factory_demo.cpp
@@ -1,52 +1,46 @@
+#include "cantera/base/Solution.h"
 #include "cantera/thermo.h"
 #include "cantera/kinetics.h"
 #include "cantera/transport.h"
 
 using namespace Cantera;
 
-// The actual code is put into a function that can be called from the main
-// program.
+// The actual code is put into a function that can be called from the main program.
 void simple_demo2()
 {
-    std::string inputFile = "gri30.yaml";
-    std::string phaseName = "gri30";
+    // Create a new 'Solution' object that provides access to ThermoPhase, Kinetics and
+    // Transport objects.
+    auto sol = newSolution("gri30.yaml", "gri30");
 
-    // Create a new ThermoPhase. Based on the phase definition in the input
-    // file, this will be an IdealGasPhase object.
-    std::unique_ptr<ThermoPhase> gas(newPhase(inputFile, phaseName));
+    // Access the ThermoPhase object. Based on the phase definition in the input file,
+    // this will reference an IdealGasPhase object.
+    auto gas = sol->thermo();
 
-    // List of phases participating in reactions (just one for homogeneous
-    // kinetics)
-    std::vector<ThermoPhase*> phases{gas.get()};
+    // Access the Kinetics object. Based on the phase definition in the input file,
+    // this will reference a GasKinetics object.
+    auto kin = sol->kinetics();
 
-    // Create the Kinetics object. Based on the phase definition in the input
-    // file, this will be a GasKinetics object.
-    std::unique_ptr<Kinetics> kin(newKinetics(phases, inputFile, phaseName));
-
-    // Set an "interesting" mixture state where we will observe non-zero reaction
-    // rates.
+    // Set an "interesting" state where we will observe non-zero reaction rates.
     gas->setState_TPX(500.0, 2.0*OneAtm, "CH4:1.0, O2:1.0, N2:3.76");
     gas->equilibrate("HP");
     gas->setState_TP(gas->temperature() - 100, gas->pressure());
 
-    // Get the net reaction rates
+    // Get the net reaction rates.
     vector_fp wdot(kin->nReactions());
     kin->getNetRatesOfProgress(wdot.data());
 
     writelog("Net reaction rates for reactions involving CO2\n");
     size_t kCO2 = gas->speciesIndex("CO2");
     for (size_t i = 0; i < kin->nReactions(); i++) {
-        if (kin->reactantStoichCoeff(kCO2, i)
-            || kin->productStoichCoeff(kCO2, i)) {
-            writelog("{:3d}  {:30s}  {: .8e}\n",
-                i, kin->reactionString(i), wdot[i]);
+        if (kin->reactantStoichCoeff(kCO2, i) || kin->productStoichCoeff(kCO2, i)) {
+            writelog("{:3d}  {:30s}  {: .8e}\n", i, kin->reactionString(i), wdot[i]);
         }
     }
     writelog("\n");
 
-    // Create a Transport object. Based on the phase definition in the input
-    // file, this will be a MixTransport object.
-    std::unique_ptr<Transport> trans(newDefaultTransportMgr(gas.get()));
+    // Access the Transport object. Based on the phase definition in the input file,
+    // this will reference a MixTransport object.
+    auto trans = sol->transport();
     writelog("T        viscosity     thermal conductivity\n");
     writelog("------   -----------   --------------------\n");
     for (size_t n = 0; n < 5; n++) {
